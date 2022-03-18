c177ac8b84efa21dc521271f906247560448332f
Ritesh Kumar <singlaritesh007@gmail.com>
2022-03-18 11:24:10 +0530

    featuring Edit post

--- my-app/src/components/EditPost.js
+++ my-app/src/components/EditPost.js
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom"
 import { useImmerReducer } from "use-immer"
 import Axios from "axios"
 import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 
 import LoadingDotsIcon from "./LoadingDotsIcon"
 
 function EditPost() {
-  const appDispatch = useContext(StateContext)
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
 
   const origionalState = {
     title: {
@@ -36,13 +38,36 @@ function EditPost() {
         draft.isFetching = false
         return
       case "updateTitle":
+        draft.title.hasError = false
         draft.title.value = action.value
         return
       case "updateBody":
+        draft.body.hasError = false
         draft.body.value = action.value
         return
       case "submitpost":
-        draft.sendCount++
+        if (!draft.title.hasError && !draft.body.hasError) {
+          draft.sendCount++
+        }
+        return
+      case "updateRequestStarted":
+        draft.isSaving = true
+        return
+      case "updateRequestFinished":
+        draft.isSaving = false
+        return
+      case "titleRules":
+        if (!draft.title.value.trim()) {
+          draft.title.hasError = true
+          draft.title.message = "Title field is Required"
+        }
+        return
+
+      case "bodyRules":
+        if (!draft.body.value.trim()) {
+          draft.body.hasError = true
+          draft.body.message = "Body field is Required"
+        }
         return
     }
   }
@@ -51,6 +76,8 @@ function EditPost() {
 
   function handelSubmit(e) {
     e.preventDefault()
+    dispatch({ type: "titleRules", value: state.title.value })
+    dispatch({ type: "bodyRules", value: state.body.value })
     dispatch({ type: "submitpost" })
   }
 
@@ -68,10 +95,13 @@ function EditPost() {
 
   useEffect(() => {
     if (state.sendCount) {
+      dispatch({ type: "updateRequestStarted" })
+
       async function fetchPost() {
         try {
-          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appDispatch.user.token })
-          alert("congrats its Successful:)")
+          const response = await Axios.post(`/post/${state.id}/edit`, { title: state.title.value, body: state.body.value, token: appState.user.token })
+          dispatch({ type: "updateRequestFinished" })
+          appDispatch({ type: "flashMessage", value: "Post was Updated." })
         } catch (e) {
           console.log("There was an error")
         }
@@ -95,17 +125,21 @@ function EditPost() {
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input value={state.title.value} onChange={(e) => dispatch({ type: "updateTitle", value: e.target.value })} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          <input onBlur={(e) => dispatch({ type: "titleRules", value: e.target.value })} value={state.title.value} onChange={(e) => dispatch({ type: "updateTitle", value: e.target.value })} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+          {state.title.hasError && <div className="alert alert-danger small liveValidateMessage">{state.title.message}</div>}
         </div>
 
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea name="body" value={state.body.value} onChange={(e) => dispatch({ type: "updateBody", value: e.target.value })} id="post-body" className="body-content tall-textarea form-control" type="text" />
+          <textarea onBlur={(e) => dispatch({ type: "bodyRules", value: e.target.value })} name="body" value={state.body.value} onChange={(e) => dispatch({ type: "updateBody", value: e.target.value })} id="post-body" className="body-content tall-textarea form-control" type="text" />
+          {state.body.hasError && <div className="alert alert-danger small liveValidateMessage">{state.body.message}</div>}
         </div>
 
-        <button className="btn btn-primary">Save Updates</button>
+        <button className="btn btn-primary" disabled={state.isSaving}>
+          <>{state.isSaving ? <>Saving...</> : <>Save Updates</>}</>
+        </button>
       </form>
     </Page>
   )
