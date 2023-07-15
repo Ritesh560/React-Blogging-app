89c1ab19561905dfc83e3d9bb9c6c564ba0b222f
Ritesh Kumar <singlaritesh007@gmail.com>
2023-07-15 22:36:53 +0530

    google form

--- my-app/src/components/body.js
+++ my-app/src/components/body.js
@@ -37,25 +37,25 @@ function Body() {
   return (
     <>
       <Page wide="true" title="Welcome">
-        <div className="row align-items-center justify-content-center">
-          <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
+        <div className="row align-items-center justify-content-center ">
+          <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5 p-5 shadowed-background">
             <form onSubmit={HandleSubmit}>
               <div className="form-group">
-                <label htmlFor="username-register" className="text-muted mb-1">
+                <label htmlFor="username-register" className="text-dark mb-1 h5">
                   <small>Username</small>
                 </label>
-                <input onChange={(e) => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+                <input onChange={(e) => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" />
               </div>
               {!isLogin && (
                 <div className="form-group">
-                  <label htmlFor="email-register" className="text-muted mb-1">
+                  <label htmlFor="email-register" className="text-dark mb-1 h5">
                     <small>Email</small>
                   </label>
                   <input onChange={(e) => setEmail(e.target.value)} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
                 </div>
               )}
               <div className="form-group">
-                <label htmlFor="password-register" className="text-muted mb-1">
+                <label htmlFor="password-register" className="text-dark mb-1 h5">
                   <small>Password</small>
                 </label>
                 <input onChange={(e) => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
@@ -63,7 +63,7 @@ function Body() {
               <button type="submit" className="py-2.5 mt-4 btn btn-lg btn-block action-button">
                 Sign up
               </button>
-              <div onClick={() => setIsLogin((prev) => !prev)} className="text-right text-primary mt-2 pointer">
+              <div onClick={() => setIsLogin((prev) => !prev)} className="text-right small text-primary mt-2 pointer" style={{ textDecoration: "underline" }}>
                 {isLogin ? "New user? Register here" : "Already have an account? Login here"}
               </div>
             </form>
